8a641c4de8107beb9ab052172cb92c76f6fb1554
Jonathan Huston <jonathan.huston@gmail.com>
2020-12-23 12:22:04 +0100

    Initial Commit

---
+++ Buzzquiz/BuzzquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzquizApp.swift
+//  Buzzquiz
+//
+//  Created by Jonathan Huston on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BuzzquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
