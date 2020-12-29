deb25ad3f3a601dd4263a8a62b822e5c42a58500
Jonathan Huston <jonathan.huston@gmail.com>
2020-12-29 01:27:37 +0100

    v1.0.0

--- Buzzquiz/BuzzquizApp.swift
+++ Buzzquiz/BuzzquizApp.swift
@@ -12,7 +12,7 @@ struct BuzzquizApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .frame(minWidth: 500, minHeight: 500, alignment: .center)
+                .frame(minWidth: 750, minHeight: 750, alignment: .center)
         }
     }
 }
--- Buzzquiz/QuestionsView.swift
+++ Buzzquiz/QuestionsView.swift
@@ -13,8 +13,6 @@ struct QuestionsView {
     
     @State private var counter = 0
     @State private var selectedAnswer = ""
-    
-//    let columns = [GridItem(.fixed(50)), GridItem(.fixed(50))]
 }
 
 extension QuestionsView: View {
@@ -28,36 +26,44 @@ extension QuestionsView: View {
                 .font(.title2)
             
             Picker(selection: $selectedAnswer, label: Text("")) {
-                if quizzes.quiz.questions[counter].answers[0].a.contains(".jpg") {
-//                    LazyVGrid(columns: columns) {
-                        ForEach(quizzes.quiz.questions[counter].answers, id:\.self) { answer in
-                                let image = answer.a.split(separator: ".")[0]
-                                Image(String(image))
-                                    .resizable()
-                                    .frame(width: 50, height: 50)
-                                    .tag(answer.a)
-                        }
-//                    }
-                } else {
-                    ForEach(quizzes.quiz.questions[counter].answers, id:\.self) { answer in
-                        Text(answer.a).tag(answer.a)
+                ForEach(quizzes.quiz.questions[counter].answers, id:\.self) { answer in
+                    if answer.a.contains(".jpg") {
+                        let image = String(answer.a.split(separator: ".")[0])
+                        Image(image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 75)
+                            .tag(answer.a)
+
+                    } else {
+                        Text(answer.a)
+                            .tag(answer.a)
+
                     }
                 }
             }
-            .padding()
             .pickerStyle(RadioGroupPickerStyle())
-                            
-            Button("Next") {
-                quizzes.quiz.characters =
-                    updatedScores(for: quizzes.quiz.characters,
-                                 answers: quizzes.quiz.questions[counter].answers,
-                                 a: selectedAnswer)
-                if counter >= (quizzes.quiz.questions.count - 1) {
-                    quizzes.bestMatch = getBestMatch(for: quizzes.quiz.characters)
-                    activeView = .result
-                } else {
-                    counter += 1
+            .padding()
+            
+            HStack {
+                Button("Next") {
+                    quizzes.quiz.characters =
+                        updatedScores(for: quizzes.quiz.characters,
+                                     answers: quizzes.quiz.questions[counter].answers,
+                                     a: selectedAnswer)
+                    if counter >= (quizzes.quiz.questions.count - 1) {
+                        quizzes.bestMatch = getBestMatch(for: quizzes.quiz.characters)
+                        activeView = .result
+                    } else {
+                        counter += 1
+                    }
                 }
+                .padding()
+                
+                Button("Quit") {
+                    activeView = .quit
+                }
+                .padding()
             }
         }
     }
